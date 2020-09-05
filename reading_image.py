c3a66b02989f00cb511b7c2acd6376332dd2e06e
fbraza <fbraza@tutanota.com>
2020-09-05 09:58:31 +0100

    function get files names

--- reading_image.py
+++ reading_image.py
@@ -36,6 +36,27 @@ def show_image(img: np.ndarray) -> None:
     plt.show()
 
 
+
+
+def get_images_yolo_box_names(path: str) -> tuple:
+    """
+    Function defined to load the names of the image and yolo bounding boxes files
+    It loads the images and yolo names separately.
+
+    Args:
+        path: string of the folder's path
+
+    Returns:
+        tuple
+    """
+    files_names = os.listdir(path) # this would raise an error if path does not exist
+    images = sorted([names for names in files_names if files_names.endswith(".jpg")])
+    bboxes = sorted([names for names in files_names if files_names.endswith(".txt")])
+    return (images, bboxes)
+
+
+
+#### Test pipeline
 test     = read_image(path = "Data/20200827_084342.jpg", opencv_color_flag = -1)
 pipeline = A.Compose([
     A.Resize(height = 256, width = 256),
@@ -44,4 +65,19 @@ pipeline = A.Compose([
 ])
 
 resized = pipeline(image = test)
-show_image(resized['image'])
+cv2.imshow("test", mat = resized['image'])
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+
+
+### TO DO
+
+"""
+    - The yolo bbox accepted by albumentation should have 4 values
+    - The yolo labels should be passed separately. 
+      The label in each txt file is the first value
+
+    - Read the image
+    - Read the yolo file
+"""
