2d4d8e69bf5387e04e5b79bebd9c320f416e9acf
fbraza <fbraza@tutanota.com>
2020-11-02 14:07:08 +0000

    Remove old io_process & upadte .gitignore

--- io_process.py
+++
@@ -1,9 +0,0 @@
-if __name__ == "__main__":
-    from lib.data_augmentation import augment_and_save
-    from lib.split_train_test import generate_yolo_inputs
-    print("Data Processing starts...\n")
-    augment_and_save("../data/original_input/All", "../data/transformed_input", 30)
-    print("Preparing data for Darknet - Yolo\n")
-    generate_yolo_inputs("../data/transformed_input", 0.75)
-    print("Data augmention, File I/O processing for Yolo are done\n")
-    print("You may now use the notebook to train your model")
